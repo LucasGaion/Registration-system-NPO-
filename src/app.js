da012faa05c19a0c90bb6e1d6c606144c63652e5
Seu Nome <seu@email.com>
2024-06-17 11:58:16 -0300

    teste

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import { generateClient } from "aws-amplify/api";
+import { createForm } from './graphql/mutations';
+
+const client = generateClient()
+
+const newForm = await client.graphql({
+    query: createForm,
+    variables: {
+        input: {
+		"areaAtuacao": "Lorem ipsum dolor sit amet",
+		"tempoEstudo": "Lorem ipsum dolor sit amet",
+		"nivelCloud": "Lorem ipsum dolor sit amet",
+		"conhecimentoAws": "Lorem ipsum dolor sit amet",
+		"conhecimentoRedes": "Lorem ipsum dolor sit amet",
+		"conhecimentoArmazenamento": "Lorem ipsum dolor sit amet",
+		"conhecimentoVirtualizacao": "Lorem ipsum dolor sit amet",
+		"conhecimentoWindows": "Lorem ipsum dolor sit amet",
+		"conhecimentoLinux": "Lorem ipsum dolor sit amet",
+		"conhecimentoSeguranca": "Lorem ipsum dolor sit amet",
+		"conhecimentoIAM": "Lorem ipsum dolor sit amet"
+	}
+    }
+});
