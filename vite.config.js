8341e7fd2bfaa3c501a9b8d98f77fee128a18d95
Seu Nome <seu@email.com>
2024-06-20 08:34:34 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
+  build: {
+    outDir: 'dist',
+    assetsDir: '.',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        admin: './admin.html',
+        forms: './forms.html',
+        user: './user.html',
+      },
+    },
+  },
   resolve: {
     alias: {
       './runtimeConfig': './runtimeConfig.browser',
     },
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  define: {
+    global: {}
   },
 });
