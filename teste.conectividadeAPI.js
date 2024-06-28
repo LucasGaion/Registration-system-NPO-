06e424db6eb67e5b1105d1f06640f790a63fd0e7
Seu Nome <seu@email.com>
2024-06-28 13:17:47 -0300

    ajuste de tela admin

--- teste.conectividadeAPI.js
+++ teste.conectividadeAPI.js
@@ -1,4 +1,11 @@
-import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import awsconfig from './src/aws-exports.js';
+import { Amplify } from 'aws-amplify';
+
+const awsconfig = {
+    aws_project_region: "us-east-1",
+    aws_appsync_graphqlEndpoint: "https://cahg6u6ll5g6howq4n4bdrbvni.appsync-api.us-east-1.amazonaws.com/graphql",
+    aws_appsync_region: "us-east-1",
+    aws_appsync_authenticationType: "API_KEY",
+    aws_appsync_apiKey: "da2-cvkka4f4efb5nj7gqvl4qiykfq"
+};
 
 Amplify.configure(awsconfig);
